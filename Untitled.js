061baf089686d6ba46d5b2287c740c6f0ff47bd6
YuriFS <yurifilipe003@gmail.com>
2021-01-27 20:01:22 +0000

    commit

---
+++ Untitled.js
@@ -0,0 +1,25 @@
+//Question 4
+var myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G']; 
+function myAlphabetLength() {
+return myAlphabet.length;
+};
+var x = myAlphabetLength();
+console.log(x);
+
+//Question 5 and 6
+var Planets = ['Mer','Ven','Ear','Mar','Jup'];
+console.log(Planets[0],Planets[1],Planets[2],Planets[3],Planets[4] );
+
+//Question 7
+var wowDatatypes = [1,'two',true];
+var a = wowDatatypes[0];
+var b = wowDatatypes[1];
+var c = wowDatatypes[2];
+console.log(a,b,c);
+
+//Questions 11 and 12
+var hello = function(){
+return 'Hello World!';
+};
+console.log(hello());
+
